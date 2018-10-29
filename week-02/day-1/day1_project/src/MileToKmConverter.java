1da209eae571be59118264cd5acb90664b6c75b8
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-29 15:29:30 +0100

    added Mile-Km converter

--- week-02/day-1/day1_project/src/MileToKmConverter.java
+++ week-02/day-1/day1_project/src/MileToKmConverter.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class MileToKmConverter {
     public static void main(String[] args) {
         // Write a program that asks for an integer that is a distance in kilometers,
         // then it converts that value to miles and prints it
+        Scanner input= new Scanner(System.in);
+        System.out.println("Írj be egy távolságot km-ben! Csak egész számokat használj!");
+        int distanceInKm=input.nextInt();
+        float distanceInMiles= (float) (distanceInKm*0.62137119224);
+        System.out.println(distanceInKm +" km mérföldben ennyi lenne: " +distanceInMiles+".");
     }
 }
