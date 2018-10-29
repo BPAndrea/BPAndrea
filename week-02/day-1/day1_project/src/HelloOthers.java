64ab2630f69e6325ecbfa4f0f991e3e7d88ff668
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-29 12:23:23 +0100

    add HelloWorld excercises

---
+++ week-02/day-1/day1_project/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        System.out.println("Helló, Áki!");
+        System.out.println("Helló, Dodo!");
+        System.out.println("Helló, Adél!");
+    }
+}
