4c7527268d3f83c6f4b05b41487d4f45f60b0eb9
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-29 13:10:33 +0100

    added Swap

---
+++ week-02/day-1/day1_project/src/Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+        a=b;
+        b=123;
+
+        System.out.println("a értéke: "+a);
+        System.out.println("b értéke: "+b);
+    }
+}
