a8099a077b213950558881565bc0ec3a387c4ab7
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-29 13:52:46 +0100

    added DefineBasicInfo

---
+++ week-02/day-1/day1_project/src/DefineBasicInfo.java
@@ -0,0 +1,17 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+        String name= "Andrea Bonnyai-Pap";
+        int expectedAgeToLive= 100;
+        double height= 1.65;
+        boolean married= true;
+        System.out.println(name);
+        System.out.println(expectedAgeToLive);
+        System.out.println(height);
+        System.out.println(married);
+    }
+}
