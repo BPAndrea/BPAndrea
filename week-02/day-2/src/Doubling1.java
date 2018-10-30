543de705ca08ba2862535f4adeb0b0d6598bed3b
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-30 12:25:20 +0100

    added Doubling

---
+++ week-02/day-2/src/Doubling1.java
@@ -0,0 +1,17 @@
+public class Doubling1 {
+
+    public static void main(String[] args) {
+        /// 1.- Create an integer variable named `baseNum` and assign the value `123` to it
+        int baseNum= 123;
+        // 3.- Print the result of `doubling(baseNum)`
+        doubling(baseNum);
+
+    }
+    // 2. - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+    public static int doubling(int number){
+        number=2*number;
+        System.out.println(number);
+        return number;
+
+    }
+}
