73a8e7f66d1abcdac92bf040776abe960ff741c5
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-30 16:56:42 +0100

    added Append-a

---
+++ week-02/day-2/src/AppendA16.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class AppendA16 {
+    // - Create an array variable named `animals`
+//   with the following content: `["koal", "pand", "zebr"]`
+// - Add all elements an `"a"` at the end
+
+    //    int[] array = new int[] {1, 2, 3};
+//    System.out.println(Arrays.toString(array));
+//    array = Arrays.copyOf(array, array.length + 1); //create new array from old array and allocate one more element
+//    array[array.length - 1] = 4;
+//    System.out.println(Arrays.toString(array))
+    public static void main(String[] args) {
+        String[] animals = {"koal", "pand", "zebr"};
+        //System.out.println(Arrays.toString(animals));
+        // create new array from old array and allocate one more element
+        animals = Arrays.copyOf(animals, animals.length + 1);
+        animals[3] = "a";
+        System.out.println(Arrays.toString(animals));
+    }
+}
