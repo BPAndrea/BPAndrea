a4f64cb176e6119da363de505d48bd55491e9ae2
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-30 15:46:41 +0100

    DoubleItems

---
+++ week-02/day-2/src/DiagonalMatrix13.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class DiagonalMatrix13 {
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
+    public static void main(String[] args) {
+//        int[][] multi = new int[4][4];
+//        for(int i=0; i<multi.length; i++){
+//            for(int j=0; j<multi[i].length; j++){
+//                if(i==j)
+//                multi[i]= 1;
+//            }
+//        }
+//        System.out.println(Arrays.toString(multi));
+    }
+}
+//for(int i=0; i<multiArray.length; i++){
+//            for (int j=0; j<multiArray[i].length; j++){
+//                multiArray[i][j]= i + " " + j;
