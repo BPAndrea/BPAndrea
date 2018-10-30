a5a4a278b139534a4a9162bd5eb8d98d43783994
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-30 12:34:03 +0100

    added Greet

---
+++ week-02/day-2/src/Greet2.java
@@ -0,0 +1,19 @@
+public class Greet2 {
+    public static void main(String[] args) {
+        //  From now on, create the usual class wrapper
+        //  and main method on your own.
+
+// 1.- Create a string variable named `al` and assign the value `Greenfox` to it
+
+// 2.- Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Greenfox`
+// 3.- Greet `al`
+        String al = "Greenfox";
+        greet(al);
+    }
+
+    public static void greet(String al) {
+        System.out.println("Greeting dear, " + al);
+
+    }
+}
