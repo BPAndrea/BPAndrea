01a4dd2ee169ef7d84bccfa829c12e3a02efeb90
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-30 13:06:17 +0100

    added sum

---
+++ week-02/day-2/src/Sum4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Sum4 {
+    public static void main(String[] args) {
+        // - Write a function called `sum` that sum all the numbers
+        //   until the given parameter and returns with an integer
+
+        sum();
+
+    }
+    public static int sum (){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("How many numbers do you wand to add?");
+        int numbersToAdd=scanner.nextInt();
+        int adding=0;
+        for(int i=0; i<numbersToAdd; i++){
+            System.out.println("Give me the "+(i+1)+". number!");
+            Scanner scanner1=new Scanner(System.in);
+            int numbers=scanner1.nextInt();
+            adding=adding+numbers;
+            System.out.println("The sum of the given numbers is: "+adding);
+        }
+        return adding;
+    }
+}
