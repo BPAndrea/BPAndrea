83b258dcdbc9897632714e679f39c8bae457bf14
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-30 14:51:50 +0100

    added Third and CompareLength

---
+++ week-02/day-2/src/CompareLength8.java
@@ -0,0 +1,15 @@
+public class CompareLength8 {
+    // - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print if `p2` has more elements than `p1`
+    public static void main(String[] args) {
+        int []q={1, 2, 3};
+        int []p2={4, 5};
+        if(q.length>p2.length){
+            System.out.println("Array \"q\" has more elements than \"p2\".");
+        }else
+        System.out.println("Array \"p2\" has more elements than \"q\".");
+    }
+}
