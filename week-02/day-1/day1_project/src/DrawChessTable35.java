9426f42899b70f4fcb29c88345c50376d041cacc
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-10-31 15:00:07 +0100

    added DrawChessTagle

---
+++ week-02/day-1/day1_project/src/DrawChessTable35.java
@@ -0,0 +1,41 @@
+public class DrawChessTable35 {
+    public static void main(String[] args) {
+        // Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+        int sizeOfTable = 9;
+        for (int i = 1; i <= sizeOfTable; i++) {
+            if ((i % 2) != 0) {
+                for (int j = 1; j <= sizeOfTable; j++) {
+                    String print = "";
+                    if (j % 2 != 0) {
+                        System.out.print(print + "%");
+                    } else {
+                        System.out.print(print + " ");
+                    }
+                }
+                System.out.println();
+            } else {
+                for (int j = 1; j <= sizeOfTable; j++) {
+                    String print = "";
+                    if (j % 2 != 0) {
+                        System.out.print(print + " ");
+                    } else {
+                        System.out.print(print + "%");
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+
+    }
+}
