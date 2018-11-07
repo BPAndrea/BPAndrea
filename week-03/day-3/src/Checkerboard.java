16463a0e16f3a4178be007d68f5ae2885960c5db
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-11-07 16:17:21 +0100

    added Checkerboard

---
+++ week-03/day-3/src/Checkerboard.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.image.ImageObserver.HEIGHT;
+import static java.awt.image.ImageObserver.WIDTH;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+    public static void mainDraw(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern.
+
+        int tileSize = WIDTH / 8;
+
+        // Fill black.
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+
+        // Color red tiles.
+        graphics.setColor(Color.RED);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i % 2 == j % 2) {
+                    graphics.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
+                }
+            }
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
+
