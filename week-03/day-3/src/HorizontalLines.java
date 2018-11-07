84e45b280bdd8ef46ecb5a496fcfcba021b2a9ec
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-11-07 11:49:13 +0100

    added HorizontalLines

--- week-03/day-3/src/HorizontalLines.java
+++ week-03/day-3/src/HorizontalLines.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-
 import java.awt.*;
-
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class HorizontalLines {
@@ -10,9 +8,17 @@ public class HorizontalLines {
         // The x and y coordinates of the line's starting point
         // and draws a 50 long horizontal line from that point.
         // Draw 3 lines with that function. Use loop for that.
+        int[] xPoint = {15, 50, 150};
+        int[] yPoint = {30, 50, 150};
+        horizontalLines(graphics, xPoint, yPoint);
+    }
 
-
-
+    public static void horizontalLines(Graphics graphics, int[] inputX, int[] inputY) {
+        for (int i = 0; i < 3; i++) {
+            int x = inputX[i];
+            int y = inputY[i];
+            graphics.drawLine(x, y, x + 50, y);
+        }
     }
 
     // Don't touch the code below
