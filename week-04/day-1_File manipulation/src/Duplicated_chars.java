7ad08b86acf44dea0a038ca9cfa933a1901850b8
Bonnyai-Pap Andrea <bonnyaipap.andrea@gmail.com>
2018-11-13 09:01:10 +0100

    added Duplicated_chars

---
+++ week-04/day-1_File manipulation/src/Duplicated_chars.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Duplicated_chars {
+    public static void main(String[] args) {
+        // Create a method that decrypts the duplicated-chars.txt
+        Path filePath = Paths.get("assets/doubled-text.txt");
+
+        try {
+            List<String> duplicatedText = Files.readAllLines(filePath);
+            //System.out.println(duplicatedText.get(0).charAt(0));
+
+            System.out.println(duplicatedText.get(0).length());
+
+            String simpleText = "";
+            for (int i = 0; i < duplicatedText.size(); i++) {
+                for (int j = 0; j < (duplicatedText.get(i).length()); j++) {
+                    if (j % 2 != 0) {
+                        simpleText += duplicatedText.get(i).charAt(j);
+                        //String new=duplicatedText.remove(0).charAt(j);
+                    }
+                }
+                simpleText+="\n";
+            }
+            System.out.println(simpleText);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
