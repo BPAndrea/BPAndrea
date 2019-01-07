19791bca18959582d2a71ab158869c4b73ebe9d3
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-01-07 13:57:29 +0100

    added Greeter

--- week-10/day1-REST/src/main/java/com/greenfox/api/controller/ApiController.java
+++ week-10/day1-REST/src/main/java/com/greenfox/api/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.greenfox.api.controller;
 
 import com.greenfox.api.model.Doubling;
 import com.greenfox.api.model.ErrorMassage;
+import com.greenfox.api.model.Greeter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,22 @@ public class ApiController {
       return new Doubling(input);
     }
   }
+
+  @GetMapping("/greeter")
+  public Object greeter(@RequestParam(value = "name", required = false) String name,
+                        @RequestParam(value = "title", required = false) String title) {
+    if ((name == null) && (title == null)) {
+      return new ErrorMassage("Please provide a name and a title!");
+    } else if (name == null) {
+      return new ErrorMassage("Please provide a name!");
+    } else if (title == null) {
+      return new ErrorMassage("Please provide a title!");
+    } else {
+      return new Greeter(name, title);
+    }
+
+
+  }
+
 
 }
--- week-10/day1-REST/src/main/java/com/greenfox/api/model/Greeter.java
+++ week-10/day1-REST/src/main/java/com/greenfox/api/model/Greeter.java
@@ -2,4 +2,19 @@ package com.greenfox.api.model;
 
 public class Greeter {
   private String welcome_message;
+
+  public Greeter() {
+  }
+
+  public Greeter(String name, String title) {
+    this.welcome_message = "Oh, hi there " + name + ", my dear student!";
+  }
+
+  public String getWelcome_message() {
+    return welcome_message;
+  }
+
+  public void setWelcome_message(String welcome_message) {
+    this.welcome_message = welcome_message;
+  }
 }
