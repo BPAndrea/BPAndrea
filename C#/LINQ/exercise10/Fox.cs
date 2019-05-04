024840e71bdcbc59d729bcd289dc7bb3263d3530
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-05-04 18:00:06 +0200

    solved exercise10

---
+++ C#/LINQ/exercise10/Fox.cs
@@ -0,0 +1,18 @@
+namespace exercise10
+{
+    public class Fox
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public string color { get; set; }
+
+        public Fox(string name, string type, string color)
+        {
+            this.name = name;
+            this.type = type;
+            this.color = color;
+        }
+
+
+    }
+}
---
+++ C#/LINQ/exercise10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fox firstFox = new Fox("Mr.Green", "student", "green");
+            Fox secondFox = new Fox("Mr.John", "teacher", "green");
+            Fox thirdFox = new Fox("Mr.Kyle", "pallida", "red");
+            Fox forthFox = new Fox("Ms.Ashley", "pallida", "green");
+            Fox fifthFox = new Fox("Ms.Ann", "student", "brown");
+            List<Fox> foxes = new List<Fox>();
+            foxes.Add(firstFox);
+            foxes.Add(secondFox);
+            foxes.Add(thirdFox);
+            foxes.Add(forthFox);
+            foxes.Add(fifthFox);
+
+            var greenFoxes = foxes.Where(f => f.color.Equals("green"));
+            foreach (var item in greenFoxes
+            )
+            {
+                System.Console.WriteLine(item.name);
+            }
+
+            var greenPallidaFoxes = foxes.Where(f => f.color == "green" && f.type == "pallida");
+            foreach (var item in greenPallidaFoxes)
+            {
+                System.Console.WriteLine("Green and Pallida: " + item.name);
+            }
+        }
+    }
+}
