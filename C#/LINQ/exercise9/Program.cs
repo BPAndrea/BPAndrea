bd87b5bb8a27e8a955bf1132f1698a845a2bf104
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-05-04 13:59:20 +0200

    solved exercise9

---
+++ C#/LINQ/exercise9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myName = new char[] { 'A', 'N', 'D', 'I' };
+            //method syntax:
+            //1. string andi = String.Join("", myName.Select(p => p.ToString()).ToArray());
+            //2. string andi = String.Join("", myName.Select(p => p.ToString()));
+            string andi = String.Join("", myName.Select(p => p));
+            System.Console.WriteLine(andi);
+
+        }
+    }
+}
