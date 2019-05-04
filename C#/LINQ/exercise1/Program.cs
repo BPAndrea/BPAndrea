42d9b12d67a96703a09d2c6cccc76e13c59db533
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-05-04 09:42:20 +0200

    LINQ exercise1

---
+++ C#/LINQ/exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the even numbers from the following array:
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            //method syntax
+            var evenNumbers = n.Where(number => number % 2 == 0);
+            foreach (var item in evenNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            //query syntax
+            var queryResult = from number in n
+                              where number % 2 == 0
+                              select number;
+            foreach (var item in queryResult)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
