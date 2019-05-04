9023193c8213c7c556cca0f352d9cdcfd7897eb4
BPAndrea <bonnyaipap.andrea@gmail.com>
2019-05-04 13:45:36 +0200

    solved exercise8

---
+++ C#/LINQ/exercise8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string.
+            string myString = "SHLAJjskWm";
+
+            //method syntax:
+            var methodResult = myString.Where(letter => char.IsUpper(letter));
+            foreach (var item in methodResult)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //query syntax:
+            var queryResult = from letter in myString
+                              where char.IsUpper(letter)
+                              select letter;
+            foreach (var item in queryResult)
+            {
+                System.Console.WriteLine("Query:" + item);
+            }
+
+        }
+    }
+}
